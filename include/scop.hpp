fa1a78b2380b86b39da6e48f10bd5fc15245125a
DropNod <prigentgwechen@gmail.com>
2024-09-04 15:04:54 +0200

    + | Added GLFW window

--- include/scop.hpp
+++ include/scop.hpp
@@ -1,4 +1,6 @@
 #pragma once
 
 #include <iostream>
+#include <stdexcept>
 #include <cstdlib>
+#include <GLFW/glfw3.h>
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,54 @@
 #include "scop.hpp"
 
-using namespace std;
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class Scop {
+    public:
+        void run() {
+            initWindow();
+            initVulkan();
+            mainLoop();
+            cleanup();
+        }
+
+    private:
+        GLFWwindow* window;
+
+        void initWindow() {
+            glfwInit();
+
+            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+            glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+            window = glfwCreateWindow(WIDTH, HEIGHT, "scop", nullptr, nullptr);
+        }
+
+        void initVulkan() {
+
+        }
+
+        void mainLoop() {
+            while (!glfwWindowShouldClose(window)){
+                glfwPollEvents();
+            }
+        }
+
+        void cleanup() {
+                glfwDestroyWindow(window);
+                glfwTerminate();
+        }
+};
 
 int main(int ac, char **av)
 {
-    cout << "Hello 42!" << endl;
-    return (EXIT_SUCCESS);
+    Scop scop;
+
+    try {
+        scop.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
